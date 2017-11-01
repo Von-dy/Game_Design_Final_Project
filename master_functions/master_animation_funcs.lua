5298b79c83724715fda3dff51e8d7e12c737c204
Von-dy <msvonderlippe@smcm.edu>
2017-11-01 15:14:23 -0400

    New Master Anim functions
    Anims

--- master_functions/master_animation_funcs.lua
+++ master_functions/master_animation_funcs.lua
@@ -1,16 +1,3 @@
---given id of boss, draw the hp
-function draw_hud(id)
- --heart boss
- local hpleft=0
- if id==1 then 
-  for v in all(boss.valves) do
-   if v.sprite==1 then rectfill(v.x,v.y,v.x+3,v.y+8,1) end
-   hpleft+=v.hp
-  end
-  rectfill(0,4,(hpleft/200)*128,6,10)
- --end heart boss drawing
- end
-end
 
 function draw_boss()
  if c==1 then 
@@ -26,59 +13,59 @@ end
 function draw_face()
  local y=0
  local x=0
+ for p in all(players) do y=p.y x=p.x end
+  draw_eyes(boss.state, 88, 56, 4, 12, 2, 8)
+  draw_lips(boss.state, 91, 72, 16, 1, 5)
+  
+  --pset(91,59,12)
+  --pset(98,59,12)
+  --if c>=1 then 
+   --line(88-4,56,88+4, 56, 2)
+   --line(100-4,56,100+4, 56, 2)
+   --pset(97,59,12)
+   --pset(92,59,12)
+   --if game.frame_counter%50<=30 then
+   --line(88-4,57,88+4, 57, 2)
+   --line(88-3,58,88+3, 58, 2)
+   --line(100-4,57,100+4, 57, 2)
+   --line(100-3,58,100+3, 58, 2)
+   --pset(92,60,12)
+   --pset(97,60,12)
+   --end
+  --end
+end
 
- for p in all(players) do
-   y=p.y
-   x=p.x
- end
-  circfill(88, 56, 4, 7)
-  circfill(100, 56, 4, 7)
+--mood, eye_x, eye_y, eye_r, distance, primary color, secondary color
+function draw_eyes(m, e_x, e_y, e_r, d, p_col, s_col)
+ local l_e_x=e_x
+ local r_e_x=e_x+d
 
- if boss.state==1 then
-   --left brow
-  line(84, 49, 90, 53, 2)
-  line(85, 50, 92, 54, 2)
-  line(85, 51, 93, 55, 8)
-  --right brow
-  line(101, 49, 98, 53, 2)
-  line(103, 50, 96, 54, 2)
-  line(103, 51, 95, 55, 8)
+ --base circle
+ circfill(e_x, e_y, e_r, 7)
+ circfill(r_e_x, e_y, e_r, 7)
+ 
+ --tracking pupils                                  -- sad pupils
+ if m<=1 then draw_pupils(e_x, e_y, e_r, d, 0) else draw_pupils(e_x, e_y, e_r, d, 1) end
 
-  --angry face
-  draw_lips(1, 91, 72, 16, 1, 5)
- elseif boss.state==0 then
-  --happy face
-  draw_lips(0, 91, 72, 16, 1, 5)
- elseif boss.state==2 then 
- --sad face
-  draw_lips(2, 91, 72, 16, 1, 5)
-  line(88-1,52,88+1, 52, 2)
-  line(88-3,53,88+3, 53, 2)
-  line(88-3,54,88+3, 54, 2)
-  line(88-4,55,88+4, 55, 2)
-  line(100-1,52,100+1, 52, 2)
-  line(100-3,53,100+3, 53, 2)
-  line(100-3,54,100+3, 54, 2)
-  line(100-4,55,100+4, 55, 2)
+ --angry eyebrows
+ if m==1 then 
+  --left brow
+ line(l_e_x-4, e_y-7, l_e_x+2, e_y-3, p_col)
+ line(l_e_x-3, e_y-6, l_e_x+4, e_y-2, p_col)
+ line(l_e_x-3, e_y-5, l_e_x+5, e_y-1, s_col)
+ --right brow
+ line(r_e_x+1, e_y-7, r_e_x-2, e_y-3, p_col)
+ line(r_e_x+3, e_y-6, r_e_x-4, e_y-2, p_col)
+ line(r_e_x+3, e_y-5, r_e_x+5, e_y-1, s_col)
+ elseif m==2 then -- sad / eyelids
+  for i=0, e_r do 
+   line(l_e_x-i, e_y-e_r+i, l_e_x+i, e_y-e_r+i, p_col)
+   line(r_e_x-i, e_y-e_r+i, r_e_x+i, e_y-e_r+i, p_col)
+  end
   pset(91,59,12)
   pset(98,59,12)
-  if c>=1 then 
-   line(88-4,56,88+4, 56, 2)
-   line(100-4,56,100+4, 56, 2)
-   pset(97,59,12)
-   pset(92,59,12)
-   if game.frame_counter%50<=30 then
-   line(88-4,57,88+4, 57, 2)
-   line(88-3,58,88+3, 58, 2)
-   line(100-4,57,100+4, 57, 2)
-   line(100-3,58,100+3, 58, 2)
-   pset(92,60,12)
-   pset(97,60,12)
-   end
-  end
-  
  end
- --pupil tracking
+
 end
 
 --left center_x, left center_y, radius, distance, mood
@@ -109,7 +96,6 @@ end
 function draw_lips(m, x, y, len, p_col, s_col)
  local l=len*2
  if m==0 then --happy mood
-  draw_pupils(88, 56, 4, 12, 0)
   for i=0, len-1 do
    pset(x+i, y-4*sin(i/l), p_col)
    pset(x+i, (y-1)-4*sin(i/l), s_col)
@@ -122,7 +108,6 @@ function draw_lips(m, x, y, len, p_col, s_col)
    end
   end
  elseif m==1 then --angry mood
-  draw_pupils(88, 56, 4, 12, 0)
   for i=0, len-1 do
     pset(x+i, y+3*sin(i/l), p_col)
     pset(x+i, (y+1)+3*sin(i/l), s_col)
@@ -136,10 +121,8 @@ function draw_lips(m, x, y, len, p_col, s_col)
      if i%2==0 and i!=0 then pset(x+i, (y+3)+3*sin(i/l), 7) end
     end
    end
-  elseif m==2 then --sad mood
-   draw_pupils(88, 56, 4, 12, 1)
-  end
  end
+end
 
  function draw_valves()
   sspr(80, 0, 8, 8, 108, 8, 8, 38)
